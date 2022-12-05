07d94c09f450efac629d5d74dc38cc697968c82a
Nguyen Duc Hien <hienndhp95@gmail.com>
2022-12-05 22:54:19 +0700

    Add script delete issue

--- src/main/java/actions/common/GlobalConstants.java
+++ src/main/java/actions/common/GlobalConstants.java
@@ -1,8 +1,8 @@
 package actions.common;
 
 public class GlobalConstants {
-    public static final String BASE_URI="https://hiennd-learning.atlassian.net";
-    public static final String PROJECT_KEY="HIENNDKB";
-    public static final String JIRA_TOKEN = "jjoBqbT4asH9XExWL22v4AFC";
-    public static final String EMAIL = "hiennd.tnhp@gmail.com";
+	public static final String BASE_URI = "https://hiennd-learning.atlassian.net";
+	public static final String PROJECT_KEY = "HIENNDKB";
+	public static final String JIRA_TOKEN = "C4X9hzEJTdfZCorPRTbSBADA";
+	public static final String EMAIL = "hiennd.tnhp@gmail.com";
 }
--- src/test/java/test/jira/JiraNewIssue.java
+++ src/test/java/test/jira/JiraNewIssue.java
@@ -1,86 +1,97 @@
 package test.jira;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import actions.common.GlobalConstants;
 import builder.IssueContentBuilder;
 import builder.TransitionContentBuilder;
 import common.BaseTest;
-import actions.common.GlobalConstants;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import model.IssueFields;
 import model.RequestCapability;
-import org.apache.commons.lang3.RandomStringUtils;
 import utils.AuthenticationHandler;
 import utils.IssueInfo;
 import utils.ProjectInfo;
 import utils.TransitionInfo;
-import java.util.Map;
-import static io.restassured.RestAssured.given;
 
 public class JiraNewIssue extends BaseTest implements RequestCapability {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 //        // App info
-        String baseUri = GlobalConstants.BASE_URI;
-        String path ="/rest/api/3/issue";
-        String projectKey = GlobalConstants.PROJECT_KEY;
-        String encodedCreStr = AuthenticationHandler.encodedCredStr(GlobalConstants.EMAIL, GlobalConstants.JIRA_TOKEN);
-        // Request Object
-        RequestSpecification request = given();
+		String baseUri = GlobalConstants.BASE_URI;
+		String path = "/rest/api/3/issue";
+		String projectKey = GlobalConstants.PROJECT_KEY;
+		String encodedCreStr = AuthenticationHandler.encodedCredStr(GlobalConstants.EMAIL, GlobalConstants.JIRA_TOKEN);
+		// Request Object
+		RequestSpecification request = given();
 //        RequestSpecification request = getAppInfo(path);
-        request.baseUri(baseUri);
-        request.header(defaultHeader);
-        request.header(acceptJSONHeader);
-        request.header(getAuthenticatedHeader.apply(encodedCreStr));
+		request.baseUri(baseUri);
+		request.header(defaultHeader);
+		request.header(acceptJSONHeader);
+		request.header(getAuthenticatedHeader.apply(encodedCreStr));
 
-        // Define body data
-        ProjectInfo projectInfo = new ProjectInfo(baseUri,projectKey);
-        String summary = "Summary | IssueContentBuilder";
-        int desiredLength=20;
-        boolean hasLetters = true;
-        boolean hasNumbers = true;
-        String randomSummary = RandomStringUtils.random(desiredLength,hasLetters,hasNumbers);
-        String taskTypeId = projectInfo.getIssueTypeId("Task");
-        IssueContentBuilder issueContentBuilder = new IssueContentBuilder();
-        String issueFieldsContent = issueContentBuilder.build(projectKey,taskTypeId, randomSummary);
+		// Define body data
+		ProjectInfo projectInfo = new ProjectInfo(baseUri, projectKey);
+		String summary = "Summary | IssueContentBuilder";
+		int desiredLength = 20;
+		boolean hasLetters = true;
+		boolean hasNumbers = true;
+		String randomSummary = RandomStringUtils.random(desiredLength, hasLetters, hasNumbers);
+		String taskTypeId = projectInfo.getIssueTypeId("Task");
+		IssueContentBuilder issueContentBuilder = new IssueContentBuilder();
+		String issueFieldsContent = issueContentBuilder.build(projectKey, taskTypeId, randomSummary);
 
-        // Send request CREATE JIRA TASK
-        Response response = request.body(issueFieldsContent).post(path);
+		// Send request CREATE JIRA TASK
+		Response response = request.body(issueFieldsContent).post(path);
 
-        // Check created task details
-        Map<String,String> responseBody = JsonPath.from(response.asString()).get();
-        final String CREATED_ISSUE_KEY =responseBody.get("key");
-        System.out.println("CREATED_ISSUE_KEY:" + CREATED_ISSUE_KEY);
-        IssueFields issueFields = issueContentBuilder.getIssueFields();
-        String expectedSummary = issueFields.getFields().getSummary();
-        String expectedStatus = "To Do";
+		// Check created task details after created
+		Map<String, String> responseBody = JsonPath.from(response.asString()).get();
+		final String CREATED_ISSUE_KEY = responseBody.get("key");
+		System.out.println("CREATED_ISSUE_KEY:" + CREATED_ISSUE_KEY);
+		IssueFields issueFields = issueContentBuilder.getIssueFields();
+		String expectedSummary = issueFields.getFields().getSummary();
+		String expectedStatus = "To Do";
 
-        // READ CREATED JIRA TASK INFO
-        IssueInfo issueInfo = new IssueInfo(baseUri,CREATED_ISSUE_KEY);
-        Map<String, String> issueInfoDetail = issueInfo.getIssueInfoDetail();
-        System.out.println("expectedSummary: " + expectedSummary);
-        System.out.println("actualSummary: " + issueInfoDetail.get("summary"));
-        System.out.println("expectedStatus: " + expectedStatus);
-        System.out.println("actualStatus: " + issueInfoDetail.get("status"));
+		// READ CREATED JIRA TASK INFO
+		IssueInfo issueInfo = new IssueInfo(baseUri, CREATED_ISSUE_KEY);
+		Map<String, String> issueInfoDetail = issueInfo.getIssueInfoDetail();
+		System.out.println("expectedSummary: " + expectedSummary);
+		System.out.println("actualSummary: " + issueInfoDetail.get("summary"));
+		System.out.println("expectedStatus: " + expectedStatus);
+		System.out.println("actualStatus: " + issueInfoDetail.get("status"));
 
-        // UPDATE CREATED JIRA TASK
-        // Get TaskID have status 'DONE'
-        TransitionContentBuilder transitionContentBuilder = new TransitionContentBuilder();
-        TransitionInfo transitionInfo = new TransitionInfo(baseUri,CREATED_ISSUE_KEY);
-        String transitionId = transitionInfo.getTransitionTypeId("Done");
-        String transitionContent = transitionContentBuilder.build(transitionId);
+		// UPDATE CREATED JIRA TASK
+		// Get TaskID have status 'DONE'
+		TransitionContentBuilder transitionContentBuilder = new TransitionContentBuilder();
+		TransitionInfo transitionInfo = new TransitionInfo(baseUri, CREATED_ISSUE_KEY);
+		String transitionId = transitionInfo.getTransitionTypeId("Done");
+		String transitionContent = transitionContentBuilder.build(transitionId);
 
-        // Send request update status of task is done by issue key
-        String getIssuePath = "/rest/api/3/issue/" + CREATED_ISSUE_KEY+"/transitions";
-        request.body(transitionContent).post(getIssuePath).then().statusCode(204);
+		// Send request update status of task is done by issue key
+		String getIssuePath = "/rest/api/3/issue/" + CREATED_ISSUE_KEY + "/transitions";
+		request.body(transitionContent).post(getIssuePath).then().statusCode(204);
 
-        // Check status of task updated
-        issueInfo = new IssueInfo(baseUri,CREATED_ISSUE_KEY);
-        issueInfoDetail = issueInfo.getIssueInfoDetail();
-        String latestIssueStatus = issueInfoDetail.get("status");
-        System.out.println("latestIssueStatus: " + latestIssueStatus);
+		// Check status of task updated
+		issueInfo = new IssueInfo(baseUri, CREATED_ISSUE_KEY);
+		issueInfoDetail = issueInfo.getIssueInfoDetail();
+		String latestIssueStatus = issueInfoDetail.get("status");
+		System.out.println("latestIssueStatus: " + latestIssueStatus);
 
-        // DELETE CREATED JIRA TASK.
-        String deleteIssuePath = "/rest/api/3/issue/" + CREATED_ISSUE_KEY;
-        request.delete(deleteIssuePath).prettyPrint();
-    }
+		// DELETE CREATED JIRA TASK.
+		String deleteIssuePath = "/rest/api/3/issue/" + CREATED_ISSUE_KEY;
+		request.delete(deleteIssuePath);
+
+		// Verify after delete
+		String getIssue = "/rest/api/3/issue/" + CREATED_ISSUE_KEY;
+		response = request.get(getIssue);
+		Map<String, List<String>> notIssueExistingIssueRes = JsonPath.from(response.body().asString()).get();
+		List<String> errorMessages = notIssueExistingIssueRes.get("errorMessages");
+		System.out.println("Return msg: " + errorMessages.get(0));
+	}
 }
