93b83d15981d69e01c1cc6b7c158296ee64b22de
Nguyen Duc Hien <hienndhp95@gmail.com>
2022-12-07 17:05:10 +0700

    Update Token

--- src/main/java/actions/common/GlobalConstants.java
+++ src/main/java/actions/common/GlobalConstants.java
@@ -3,7 +3,7 @@ package actions.common;
 public class GlobalConstants {
 	public static final String BASE_URI = "https://hiennd-learning.atlassian.net";
 	public static final String PROJECT_KEY = "HIENNDKB";
-	public static final String JIRA_TOKEN = "k8L8alRyJPZR0KnSWtFj8335";
+	public static final String JIRA_TOKEN = "IUEL9XZU50I0KY5rgEqn0861";
 	public static final String EMAIL = "hiennd.tnhp@gmail.com";
 	public static final String PROJECT_PATH = System.getProperty("user.dir");
 }
