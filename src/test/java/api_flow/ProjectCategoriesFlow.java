0ef97b4ae3f7e564015845dfaa62e13156b107a9
Nguyen Duc Hien <hienndhp95@gmail.com>
2022-12-14 14:42:20 +0700

    Add Allure Report comment

--- src/test/java/api_flow/ProjectCategoriesFlow.java
+++ src/test/java/api_flow/ProjectCategoriesFlow.java
@@ -1,12 +1,12 @@
 package api_flow;
 
 import actions.common.GlobalConstants;
+import io.qameta.allure.Step;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
 import org.testng.Assert;
-
 import java.util.List;
 import java.util.Map;
 
@@ -24,6 +24,7 @@ public class ProjectCategoriesFlow {
         this.description = description;
     }
 
+    @Step("Get all project categories")
     public void getAllProjectCategories(){
         Response response = request.get(issuePathPrefix);
         List<Map<String,String>> projectCategories = JsonPath.from(response.getBody().asString()).get();
@@ -33,11 +34,12 @@ public class ProjectCategoriesFlow {
         }
     }
 
+    @Step("Get project category by ID")
     public void getProjectCategoryByID(){
         String getProjectCategoryByIdPath = issuePathPrefix + "/" + projectID;
         response = request.get(getProjectCategoryByIdPath);
     }
-
+    @Step("Creating jira project category")
     public void createProjectCategory(){
         JSONObject data = new JSONObject();
         data.put("name",randomName);
@@ -47,6 +49,7 @@ public class ProjectCategoriesFlow {
         projectID = responseBody.get("id");
     }
 
+    @Step("Updating project category")
     public void updateProjectCategory(){
         String updateProjectCategoryPath = issuePathPrefix + "/" + projectID;
         JSONObject data = new JSONObject();
@@ -55,6 +58,7 @@ public class ProjectCategoriesFlow {
         response = request.body(data.toJSONString()).put(updateProjectCategoryPath);
     }
 
+    @Step("Deleting project category")
     public void deleteProjectCategory(){
         String path = issuePathPrefix + "/" + projectID;
         request.delete(path);
@@ -63,6 +67,7 @@ public class ProjectCategoriesFlow {
         Assert.assertEquals(responseAfterDelete,"Project category with id: " + projectID + " not found.");
     }
 
+    @Step("Verifying project category")
     public void verifyResponseDataDetail(){
         Map<String,String> projectCategoryBody = JsonPath.from(response.getBody().asString()).get();
         Assert.assertEquals(projectCategoryBody.get("self"), GlobalConstants.BASE_URI + issuePathPrefix + "/" + projectID);
